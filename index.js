b3f9a6b49a0b898879bd64068cb26a4f7d8d6e37
eethan1 <call132call132@gmail.com>
2021-01-21 00:16:08 +0800

    reconstruct, add menu ramen reply; modify gingin & some words

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const fetch = require('node-fetch');
 const crypto = require('crypto');
+const { renameSync } = require('fs');
 require('dotenv').config();
 
 const app = new Koa();
@@ -11,11 +12,16 @@ const app = new Koa();
 const channelSecret = process.env.CHANNEL_SECRET; // Channel secret string
 const channelAccessToken = process.env.CHANNEL_ACCESS_TOKEN;
 
+
+app.use(require('koa-static')('./image'));
+
 app.use(bodyParser());
 
 app.use(async (ctx, next) => {
     console.log(ctx.request.headers);
     console.log(ctx.request.body);
+    console.log(ctx.request.origin);
+    console.log(ctx.request.path);
     next();
 });
 
@@ -36,15 +42,51 @@ app.use(async (ctx, next) => {
 app.use(async (ctx) => {
     const replyToken = ctx.request.body.events?.[0].replyToken;
     const text = ctx.request.body.events?.[0].message?.text;
+    console.log('replyToken & text');
+    console.log(replyToken);
+    console.log(ctx.request.body.events?.[0].message);
     if(replyToken && text) {
-        reply(replyToken, text);
-        ctx.response.status = 200;
+        let msg;
+        switch(text) {
+            case 'Ramen!':
+                console.log('Ramen');
+                let path = `https://${ctx.request.host}${getRamen(text)}`;
+                console.log(path);
+                msg = {
+                    "type": "image",
+                    "originalContentUrl": path,
+                    "previewImageUrl": path+".preview",
+                }
+                replyMSG(replyToken, msg);
+                ctx.response.status = 200;
+                break;
+            default:
+                let reply = await getBullshit(text);
+                msg = {
+                    "type": "text",
+                    "text": reply,
+                }
+                replyMSG(replyToken, msg);
+                ctx.response.status = 200;
+                break;
+        }
+    }else{
+        ctx.body = 'Errrrrr';
+        ctx.response.status = 404;
     }
 });
 
-async function reply(replyToken, text) {
+
+
+function getRamen(text) {
+    let i = new Date();
+    i = i % 10;
+    return `/${i}.jpg`;
+}
+
+async function getBullshit(text) {
     let bullshitContent = {
-        Topic:'GinGin 很厲害',
+        Topic:' 俊俊很棒',
     };
     let bullshit = await fetch('https://api.howtobullshit.me/bullshit', {
         method: 'POST',
@@ -56,17 +98,17 @@ async function reply(replyToken, text) {
         .then(res => res.text())
         .then(r=>r);
     bullshit = bullshit.replace(/(&nbsp;|\<br\>)/g,'');
-    if(/(GIN\ *GIN|俊俊)/i.exec(text)) 
-        bullshit = 'A, '+bullshit;
+    if(/(GIN\ *GIN|俊\ *俊)/i.exec(text)) 
+        bullshit = '噢, '+bullshit;
     else 
-        bullshit = '你是說「'+bullshit+'」嗎?';
+        bullshit = 'Puipui, 你是說「'+bullshit+'」嗎?';
+    return bullshit;
+}
 
+async function replyMSG(replyToken, MSG) {
     const content = {
         replyToken: replyToken,
-        messages:  [{
-            "type": "text",
-            "text": bullshit,
-        }],
+        messages:  [MSG],
     }
     fetch('https://api.line.me/v2/bot/message/reply', {
         method: 'POST',
@@ -80,5 +122,4 @@ async function reply(replyToken, text) {
     .then(j => console.log(j)); 
 }
 
-
-app.listen(7130);
+app.listen(process.env.PORT);
